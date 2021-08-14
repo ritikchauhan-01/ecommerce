a009dbf79a4ee6a52ea91376175d20c8d351f6df
ritik <ritikcn05@gmail.com>
2021-08-14 22:40:56 +0530

    Adding fucntionality to the checkout.html Sending the address the backend and hide the unnecessary things when the user is logged in. And the hiding of payment button if address is not filled. And adding the payment configurations, the urls, sending the data to backend by using the post api.

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from . models import *
 from django.http import JsonResponse
 import json
+import datetime
 
 # Create your views here.
 
@@ -13,7 +14,7 @@ def store(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total': 0,'get_cart_items': 0 }
+        order = {'get_cart_total': 0,'get_cart_items': 0 ,'shipping':False}
         cartItems = order['get_cart_items']
 
 
@@ -33,7 +34,7 @@ def cart(request):
         order = {'get_cart_total': 0,'get_cart_items': 0 }
         cartItems = order['get_cart_items']
 
-    context = {'items':items,'order':order,'cartItems':cartItems}
+    context = {'items':items,'order':order,'cartItems':cartItems,'shipping':False}
     return render(request,'store/cart.html',context)
 
 def checkout(request):
@@ -44,7 +45,7 @@ def checkout(request):
         cartItems = order.get_cart_items
     else:
         item = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0,'shipping':False}
         cartItems = order['get_cart_items']
         
     context = {'items':items, 'order':order,'cartItems':cartItems}
@@ -75,3 +76,33 @@ def updateItem(request):
         orderItem.delete()
 
     return JsonResponse('Item was added', safe=False)
+
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == float(order.get_cart_total):
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+
+    else:
+        print("User is not logged in")
+
+    return JsonResponse('Pament complete',safe=False)
