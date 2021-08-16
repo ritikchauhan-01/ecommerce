3847858df89d6fda2e69c37e1bd05722f7dfbadf
ritik <ritikcn05@gmail.com>
2021-08-16 13:28:44 +0530

    adding the cookies ( we use these cookies when the user is not logged in to work the site like when the user is logged in )
    added the paypal integeration for payment when the user checkout the order.

--- static/js/cart.js
+++ static/js/cart.js
@@ -9,7 +9,7 @@ for(var i=0;i<updateBtns.length;i++){
         console.log('USER:',user)
         if(user ==='AnonymousUser')
         {
-            console.log('Not logged in')
+            addCookieItem(productId,action)
         }
         else
         {
@@ -17,6 +17,35 @@ for(var i=0;i<updateBtns.length;i++){
         }
     })
 }
+
+function addCookieItem(productId,action){
+    console.log('User Not logged in',productId)
+
+    if (action == 'add'){
+        if(cart[productId]==undefined){
+            cart[productId] = {'quantity':1}
+        }
+        else{
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if (action == 'remove'){
+        cart[productId]['quantity'] -= 1
+
+        if(cart[productId]['quantity'] <= 0){
+            console.log('Remove item')
+            delete cart[productId]
+        }
+    }
+
+    console.log('Cart:',cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
+
+}
+
+
 
 function updateUserOrder(productId,action){
     console.log('user is logged in, sending data..')
